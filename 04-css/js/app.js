bb06efcf2a2679dfef9a8c40c8ba053b60291ee4
pipe <felipefefex1122@gmail.com>
2025-02-18 17:13:35 -0500

    i add new things

--- 04-css/js/app.js
+++ 04-css/js/app.js
@@ -1,4 +1,8 @@
 $(function () {
+   //count Tasks//
+   countTasks()
+   countRemains()
+   //add Task//
    $('footer').on('click', '#add', function () {
       if ($('#input-task').val().length > 0) {
 
@@ -9,9 +13,37 @@ $(function () {
       </article>'
          $('section.list').append($task)
          $('#input-task').val('')
+         countTasks()
+         countRemains()
       }
       else {
          alert('please! Enter a Task')
       }
    })
+   $('body').on('click', 'input[type=checkbox]', function(){
+    
+      if($(this).prop('checked')) {
+         $(this).parent().addClass('checked')
+      } else {
+         $(this).parent().removeClass('checked')
+      }
+      countRemains()
+   })
+   //remove task
+   $('body').on('click', 'article button', function(){
+      $(this).closest('article').remove()
+      countTasks()
+      countRemains()
+   })
 })
+
+function countTasks() {
+   $('.num-tasks').text($('article').length)
+   $('.title-tasks').text(('article').length > 1 ? 'Tasks' : 'Task')
+}
+//Count remains
+function countRemains() {
+   $remain = Math.abs($('.checked').length - $('article').length)
+   $('.num-remains').text($remain)
+   $('.title-remains').text($remain >1?'Remains':'Remain')
+}
