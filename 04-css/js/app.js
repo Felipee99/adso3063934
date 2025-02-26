5a14dd797ef48096017194d0d930e36dc4dad3b0
pipe <felipefefex1122@gmail.com>
2025-02-25 20:22:11 -0500

    i add new thing like css, js and html for my aplication

--- 04-css/js/app.js
+++ 04-css/js/app.js
@@ -1,7 +1,17 @@
 $(function () {
-   //count Tasks//
-   countTasks()
-   countRemains()
+
+   // check localStorage
+   if (localStorage.getItem('todolist') != null) {
+      $('.list').html(localStorage.getItem('todolist'))
+      countTasks()
+      countRemains()
+   } else {
+      //count Tasks//
+      countTasks()
+      countRemains()
+   }
+
+
    //add Task//
    $('footer').on('click', '#add', function () {
       if ($('#input-task').val().length > 0) {
@@ -20,17 +30,19 @@ $(function () {
          alert('please! Enter a Task')
       }
    })
-   $('body').on('click', 'input[type=checkbox]', function(){
-    
-      if($(this).prop('checked')) {
+   $('body').on('click', 'input[type=checkbox]', function () {
+      // if checked
+      if ($(this).prop('checked')) {
+         $(this).attr('checked', true)
          $(this).parent().addClass('checked')
       } else {
+         $(this).attr('checked', false)
          $(this).parent().removeClass('checked')
       }
       countRemains()
    })
    //remove task
-   $('body').on('click', 'article button', function(){
+   $('body').on('click', 'article button', function () {
       $(this).closest('article').remove()
       countTasks()
       countRemains()
@@ -45,5 +57,17 @@ function countTasks() {
 function countRemains() {
    $remain = Math.abs($('.checked').length - $('article').length)
    $('.num-remains').text($remain)
-   $('.title-remains').text($remain >1?'Remains':'Remain')
+   $('.title-remains').text($remain > 1 ? 'Remains' : 'Remain')
+   // set localStorage
+   localStorage.setItem('todolist', $('.list').html())
 }
+function reset() {
+   $('.list').html('')
+   localStorage.removeItem('Todo list')
+   countTasks()
+   countRemains()
+}
+
+$('main').on('click', "#reset", function (){
+   reset()
+})
